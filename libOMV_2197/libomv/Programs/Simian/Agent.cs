301829e32fcc27846d219afc830e9e58da6a3b4e
teravus <teravus@7c4940e8-6ff7-4914-b775-378afef7cd2e>
2008-09-06 08:04:48 +0000

    * LibSL that goes with OpenSim revision r6143 * OpenMV r2197
    git-svn-id: file:///var/svn/opensim-libs@57 7c4940e8-6ff7-4914-b775-378afef7cd2e

---
+++ libOMV_2197/libomv/Programs/Simian/Agent.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using OpenMetaverse;
+using OpenMetaverse.Packets;
+
+namespace Simian
+{
+    public class Agent
+    {
+        public UUID AgentID;
+        public UUID SessionID;
+        public UUID SecureSessionID;
+        public uint CircuitCode;
+        public string FirstName;
+        public string LastName;
+        public Avatar Avatar = new Avatar();
+        public int Balance;
+        public bool Running;
+        public int TickFall;
+        public int TickJump;
+        public int TickLastPacketReceived;
+        public AgentManager.ControlFlags ControlFlags = AgentManager.ControlFlags.NONE;
+        public AnimationSet Animations = new AnimationSet();
+        public Dictionary<UUID, InventoryObject> Inventory = new Dictionary<UUID, InventoryObject>();
+        public Dictionary<UUID, InventoryObject> Library = new Dictionary<UUID, InventoryObject>();
+        public Dictionary<WearableType, UUID> Wearables = new Dictionary<WearableType, UUID>();
+        public byte[] VisualParams = new byte[218];
+        // TODO: Replace byte with enum
+        public byte State;
+        public PrimFlags Flags;
+        public UUID InventoryRoot;
+        public UUID InventoryLibRoot;
+    }
+}
